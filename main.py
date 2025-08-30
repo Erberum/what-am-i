00fa3259f2a5040a4814266442b1b8040e70b181
Elisei Kambalin <erberum@gmail.com>
2025-08-31 01:38:37 +1000

    resolved dependencies conflicts

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+from blockchain import BlockChain
+
 app = FastAPI()
+
+blockchain = BlockChain()
 
 
 @app.get('/')
 async def root():
     return {'test': 'test'}
+
+
+@app.get('/blockchain/{block_index}')
+async def root(block_index: int):
+    pass
